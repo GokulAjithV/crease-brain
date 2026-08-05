6a02a91e28643d8da6af0f95f61b6ee88178e611
GokulAjithV <gokulvasu225@gmail.com>
2026-08-05 22:23:18 +0530

    fix: load .env

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ and registers all route modules.
 """
 
 import logging
+from dotenv import load_dotenv
+load_dotenv()
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
